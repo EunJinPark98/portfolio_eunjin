79271bc3cd65c31c7ed460f2627b6ba8e8f16ee8
EJP <obliviscor29@naver.com>
2023-10-24 23:50:29 +0900

    라우터 경로도 수정

--- src/App.js
+++ src/App.js
@@ -18,46 +18,46 @@ function App() {
       <Container fluid>
         <Row>
           <Col xs={3} className='menu-bar'>
-            <h1 className='title mb-2' onClick={()=>{navigate('/')}}>Eunjin's<br/>Portfolio</h1>
+            <h1 className='title mb-2' onClick={()=>{navigate('/portfolio_eunjin')}}>Eunjin's<br/>Portfolio</h1>
             <p className='mb-5'><img className="menu-photo" src="https://eunjinpark98.github.io/portfolio_eunjin/memoji1.png"></img></p>
-            <p className='menu-profile' onClick={()=>{navigate('/about')}}>Profile</p>
-            <p className='menu-skills' onClick={()=>{navigate('/skills')}}>Skills</p>
-            <p className='menu-projects' onClick={()=>{navigate('/projects')}}>Projects</p>
-            <p className='menu-archive' onClick={()=>{navigate('/archive')}}>Archive</p>
+            <p className='menu-profile' onClick={()=>{navigate('/portfolio_eunjin/about')}}>Profile</p>
+            <p className='menu-skills' onClick={()=>{navigate('/portfolio_eunjin/skills')}}>Skills</p>
+            <p className='menu-projects' onClick={()=>{navigate('/portfolio_eunjin/projects')}}>Projects</p>
+            <p className='menu-archive' onClick={()=>{navigate('/portfolio_eunjin/archive')}}>Archive</p>
           </Col>
           <Col>
             <Row>
               <Col>
                 <Routes>
-                  <Route path="/" element={<Main />} />
+                  <Route path="/portfolio_eunjin" element={<Main />} />
                 </Routes>
               </Col>
             </Row>
             <Row>
               <Col style={{padding : '0'}}>
               <Routes>
-                <Route path="/about" element={<About />} />
+                <Route path="/portfolio_eunjin/about" element={<About />} />
               </Routes>
               </Col>
             </Row>
             <Row>
               <Col>
               <Routes>
-                <Route path="/skills" element={<Skills />} />
+                <Route path="/portfolio_eunjin/skills" element={<Skills />} />
               </Routes>
               </Col>
             </Row>
             <Row>
               <Col>
               <Routes>
-                <Route path="/projects" element={<Projects />} />
+                <Route path="/portfolio_eunjin/projects" element={<Projects />} />
               </Routes>
               </Col>
             </Row>
             <Row>
               <Col>
               <Routes>
-                <Route path="/archive" element={<Archive />} />
+                <Route path="/portfolio_eunjin/archive" element={<Archive />} />
               </Routes>
               </Col>
             </Row>
