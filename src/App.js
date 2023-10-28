af3ab3f2352be36444c99d2bbd7c1f34c3ce0122
EJP <obliviscor29@naver.com>
2023-10-28 16:02:54 +0900

    모바일 메인

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Nav, NavDropdown, Navbar, Row } from 'react-bootstrap';
 import './App.css';
 import About from './About';
 import { Route, Routes, useNavigate } from 'react-router-dom';
@@ -17,13 +17,31 @@ function App() {
     <div className="App">
       <Container fluid>
         <Row>
+          <Col className='menu-bar-mo'>
+            <Navbar expand="lg" className="bg-body-tertiary">
+              <Container>
+                <Navbar.Brand className='title' onClick={() => { navigate('/portfolio_eunjin') }}>Eunjin's Portfolio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link onClick={() => { navigate('/portfolio_eunjin/about') }}>Profile</Nav.Link>
+                    <Nav.Link onClick={() => { navigate('/portfolio_eunjin/skills') }}>Skills</Nav.Link>
+                    <Nav.Link onClick={() => { navigate('/portfolio_eunjin/projects') }}>Projects</Nav.Link>
+                    <Nav.Link onClick={() => { navigate('/portfolio_eunjin/archive') }}>Archive</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+          </Col>
+        </Row>
+        <Row>
           <Col xs={3} className='menu-bar'>
-            <h1 className='title mb-2' onClick={()=>{navigate('/portfolio_eunjin')}}>Eunjin's<br/>Portfolio</h1>
+            <h1 className='title mb-2' onClick={() => { navigate('/portfolio_eunjin') }}>Eunjin's<br />Portfolio</h1>
             <p className='mb-5'><img className="menu-photo" src="https://eunjinpark98.github.io/portfolio_eunjin/memoji1.png"></img></p>
-            <p className='menu-profile' onClick={()=>{navigate('/portfolio_eunjin/about')}}>Profile</p>
-            <p className='menu-skills' onClick={()=>{navigate('/portfolio_eunjin/skills')}}>Skills</p>
-            <p className='menu-projects' onClick={()=>{navigate('/portfolio_eunjin/projects')}}>Projects</p>
-            <p className='menu-archive' onClick={()=>{navigate('/portfolio_eunjin/archive')}}>Archive</p>
+            <p className='menu-profile' onClick={() => { navigate('/portfolio_eunjin/about') }}>Profile</p>
+            <p className='menu-skills' onClick={() => { navigate('/portfolio_eunjin/skills') }}>Skills</p>
+            <p className='menu-projects' onClick={() => { navigate('/portfolio_eunjin/projects') }}>Projects</p>
+            <p className='menu-archive' onClick={() => { navigate('/portfolio_eunjin/archive') }}>Archive</p>
           </Col>
           <Col>
             <Row>
@@ -34,31 +52,31 @@ function App() {
               </Col>
             </Row>
             <Row>
-              <Col style={{padding : '0'}}>
-              <Routes>
-                <Route path="/portfolio_eunjin/about" element={<About />} />
-              </Routes>
+              <Col style={{ padding: '0' }}>
+                <Routes>
+                  <Route path="/portfolio_eunjin/about" element={<About />} />
+                </Routes>
               </Col>
             </Row>
             <Row>
               <Col>
-              <Routes>
-                <Route path="/portfolio_eunjin/skills" element={<Skills />} />
-              </Routes>
+                <Routes>
+                  <Route path="/portfolio_eunjin/skills" element={<Skills />} />
+                </Routes>
               </Col>
             </Row>
             <Row>
               <Col>
-              <Routes>
-                <Route path="/portfolio_eunjin/projects" element={<Projects />} />
-              </Routes>
+                <Routes>
+                  <Route path="/portfolio_eunjin/projects" element={<Projects />} />
+                </Routes>
               </Col>
             </Row>
             <Row>
               <Col>
-              <Routes>
-                <Route path="/portfolio_eunjin/archive" element={<Archive />} />
-              </Routes>
+                <Routes>
+                  <Route path="/portfolio_eunjin/archive" element={<Archive />} />
+                </Routes>
               </Col>
             </Row>
           </Col>
--- src/Main.js
+++ src/Main.js
@@ -11,9 +11,9 @@ function Main() {
 
   const [displayIntro3, setDisplayIntro3] = useState(false);
   const intro3 = `빠르게 성장하는 개발자 박은진입니다. <br />
-    새로운 것을 배워나가는 것을 좋아하며 개발에 흥미와 애정이 큽니다. <br />
+    새로운 것에 도전하는 것을 좋아하며 개발에 열정과 애정이 큽니다. <br />
     배우고 깨달은 것들을 항상 기록하고 있으며, <br />
-    사람을 엄청 좋아해서 인간관계에 대한 스트레스가 거의 없는 편입니다.`;
+    사람을 좋아해서 인간관계를 소중히 생각하는 편입니다.`;
 
   useEffect(() => {
     if (intro1Index < intro1.length) {
@@ -46,6 +46,7 @@ function Main() {
 
   return (
     <div className="intro">
+      <img className="main-photo-mo mb-2" src="https://eunjinpark98.github.io/portfolio_eunjin/memoji1.png"></img>
       <h1 className="intro-1 mb-2">{displayIntro1}</h1>
       <h3 className="intro-2 mb-5">{displayIntro2}</h3>
       <p className={displayIntro3 ? "intro-3 show" : "intro-3"} dangerouslySetInnerHTML={{ __html: intro3 }}></p>
