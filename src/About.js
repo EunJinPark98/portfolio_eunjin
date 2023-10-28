162ffe08bf80060f58c949b51804348386dc914d
EJP <obliviscor29@naver.com>
2023-10-28 16:32:08 +0900

    profile 모바일

--- src/About.js
+++ src/About.js
@@ -3,36 +3,38 @@ import { Col, Row } from "react-bootstrap";
 function About(){
     return (
         <div className="about-fr">
-            <Row>
-                <Col>
-                    <Row className="info">
-                        <Col>
-                            <div className="photo-fr">
-                                <div className="flip">
-                                    <img className="photo2" src="https://eunjinpark98.github.io/portfolio_eunjin/photo2.png" />
-                                    <img className="photo" src="https://eunjinpark98.github.io/portfolio_eunjin/photo.jpg" />
-                                </div>
-                            </div>
-                        </Col>
-                        <Col className="gray">
-                            <p>이름</p>
-                            <p>생년월일</p>
-                            <p>주소지</p>
-                            <p>학력</p>
-                            <p>연락처</p>
-                            <p>이메일</p>
-                        </Col>
-                        <Col>
-                            <p>박은진</p>
-                            <p>980609</p>
-                            <p>울산광역시 북구</p>
-                            <p>울산대학교(사회복지학전공)</p>
-                            <p>010-5512-4219</p>
-                            <p>obliviscor29@gamil.com</p>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
+            <div className="profile-photo mb-4">
+                <div className="photo-fr">
+                    <div className="flip">
+                        <img className="photo2" src="https://eunjinpark98.github.io/portfolio_eunjin/photo2.png" />
+                        <img className="photo" src="https://eunjinpark98.github.io/portfolio_eunjin/photo.jpg" />
+                    </div>
+                </div>
+            </div>
+            <div className="profile-info">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>이름</th>
+                            <th>생년월일</th>
+                            <th>주소지</th>
+                            <th>학력</th>
+                            <th>연락처</th>
+                            <th>이메일</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>박은진</td>
+                            <td>980609</td>
+                            <td>울산광역시</td>
+                            <td>울산대학교(사회복지학전공)</td>
+                            <td>010-5512-4219</td>
+                            <td>obliviscor29@gamil.com</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
     
