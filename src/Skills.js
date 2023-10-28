73b10529df4a065e7e42ec4c0d06a2a721381fb4
EJP <obliviscor29@naver.com>
2023-10-28 17:36:59 +0900

    skills 모바일

--- src/Skills.js
+++ src/Skills.js
@@ -3,6 +3,47 @@ import { Col, Row } from "react-bootstrap";
 function Skills(){
     return(
         <>
+        {/* 모바일 */}
+        <Row className="skill-mo">
+            <Col>
+                <Row>
+                    <Col className="skill-box font-box mb-5">
+                        <h1 className="skill-cate">Frontend</h1>
+                        <p><img className="front-logo3" src="https://eunjinpark98.github.io/portfolio_eunjin/htmlcssjs.jpeg"></img></p>
+                        <img className="front-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/jQuery.png"></img>
+                        <img className="front-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/react.png"></img>
+                        <img className="front-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/vue.png"></img>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="skill-box back-box mb-5">
+                        <h1 className="skill-cate">Backend</h1>
+                        <p><img className="java-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/Java.png"></img></p>
+                        <img className="back-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/spring.svg" style={{marginBottom : '10px'}}></img>
+                        <img className="back-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/springboot.png"></img> <br />
+                        <img className="back-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/oracle.png"></img>
+                        <img className="back-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/MySQL.png"></img>
+                    </Col>    
+                </Row>
+                <Row>
+                    <Col className="skill-box version-box mb-5">
+                        <h1 className="skill-cate">Version Control</h1>
+                        <img className="version-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/git.png"></img>
+                        <img className="version-logo2" src="https://eunjinpark98.github.io/portfolio_eunjin/github.png"></img>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="skill-box design-box">
+                        <h1 className="skill-cate">Design</h1>
+                        <img className="design-logo" src="https://eunjinpark98.github.io/portfolio_eunjin/figma.png"></img>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+
+
+
+        {/* pc */}
         <Row className="skill-fr">
             <Col className="skill-box font-box">
                 <h1 className="skill-cate">Frontend</h1>
