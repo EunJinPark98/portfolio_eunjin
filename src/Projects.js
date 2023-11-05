23144f0a1d3b64f601d39a22cecfbe86f90d06bf
EJP <obliviscor29@naver.com>
2023-11-05 20:52:58 +0900

    프로젝트 모바일 수정

--- src/Projects.js
+++ src/Projects.js
@@ -15,9 +15,9 @@ function Projects(){
                     <h5>쇼핑몰 웹 사이트</h5>
                     <p>젝시믹스 컨텐츠와 현대백화점몰 디자인을 결합해서 만든 웹 사이트</p>
                     <p>작업기간 : 2023.06.19~2023.07.03 (2주)</p>
-                    <p>CSS flex 레이아웃 구성 <br />
-                        jQuery 메뉴 탭, 등장 효과, 
-                        Slick 이미지 슬라이드<br />
+                    <p>개발자 : 박은진 (1명)</p>
+                    <p>jQuery를 활용한 메뉴 탭, 등장 효과<br />
+                        Slick 플러그인을 활용한 이미지 슬라이드<br />
                         웹 표준을 준수하여 기존 웹 사이트에서 많이 사용되는 기능들을 구현했습니다.
                     </p>
                     <div className="badge mb-2">
@@ -42,9 +42,10 @@ function Projects(){
                     <h5>회사소개 반응형 웹 사이트</h5>
                     <p>카카오엔터프라이즈 컨텐츠와 카카오페이 디자인을 결합해서 만든<br />반응형 웹 사이트</p>
                     <p>작업기간 : 2023.07.03~2023.07.24 (2주)</p>
+                    <p>개발자 : 박은진 (1명)</p>
                     <p>모바일 320px,  태블릿 768px,  PC 1920px <br />
-                        Figma를 사용한 반응형 웹 디자인<br />
-                        jQuery를 사용한 각종 탭 효과 및 슬라이드<br />
+                        Figma를 활용한 반응형 웹 디자인,<br />
+                        jQuery를 활용한 각종 탭 효과를 넣었습니다. <br />
                     </p>
                     <div className="badge m-2">
                         <img src="https://eunjinpark98.github.io/portfolio_eunjin/badge_html.svg" />
@@ -67,8 +68,9 @@ function Projects(){
                     <h5>그린의 서재</h5>
                     <p>스프링부트 웹 개발 프로젝트</p>
                     <p>작업기간 : 2023.09.04~2023.09.25 (3주)</p>
-                    <p> 로그인 / 회원가입<br />
-                        장바구니, 상품 구매, 상품 등록, 상품 조회, 페이징
+                    <p>개발자 : 박은진 (1명)</p>
+                    <p> 로그인 / 회원가입 (시큐리티)<br />
+                        상품 상세보기, 장바구니, 구매, 상품 등록, 조회, 페이징
                     </p>
                     <div className="badge">
                         <img src="https://eunjinpark98.github.io/portfolio_eunjin/badge_html.svg" />
@@ -94,7 +96,8 @@ function Projects(){
                     <h5>GREEN MOA</h5>
                     <p>학급 커뮤니티</p>
                     <p>작업기간 : 2023.10.11~2023.11.10 (한 달)</p>
-                    <p>캐릭터 접속, 이동, 채팅 가능한 메타버스 공간<br/>
+                    <p>개발자 : 박은진, 김진우, 신협진, 이윤주 (4명)</p>
+                    <p>웹 소켓으로 캐릭터 접속, 이동, 채팅 가능한 메타버스 공간<br/>
                         게시판, 캘린더, 쪽지, 투두리스트</p>
                     <div className="badge">
                         <img src="https://eunjinpark98.github.io/portfolio_eunjin/badge_html.svg" />
@@ -119,10 +122,9 @@ function Projects(){
                 <div className="project-cate">Front-end</div>
                 <Col>
                     <h5>포트폴리오</h5>
-                    <p></p>
-                    <p>작업기간 : 2023.10.21~2023.10.29 (8일)</p>
+                    <p>작업기간 : 2023.10.21~2023.10.24 (4일)</p>
+                    <p>개발자 : 박은진 (1명)</p>
                     <p>React로 제작한 포트폴리오 사이트<br />
-                        모바일 320px PC 1024px 반응형
                     </p>
                     <div className="badge mb-3">
                         <img src="https://eunjinpark98.github.io/portfolio_eunjin/badge_react.svg" />
@@ -256,7 +258,7 @@ function Projects(){
                     <p>학급 커뮤니티</p>
                     <p>작업기간 : 2023.10.11~2023.11.10 (한 달)</p>
                     <p>개발자 : 박은진, 김진우, 신협진, 이윤주 (4명)</p>
-                    <p>캐릭터 접속, 이동, 채팅 가능한 메타버스 공간<br/>
+                    <p>웹 소켓으로 캐릭터 접속, 이동, 채팅 가능한 메타버스 공간<br/>
                         게시판, 캘린더, 쪽지, 투두리스트</p>
                     <div className="badge">
                         <img src="https://eunjinpark98.github.io/portfolio_eunjin/badge_html.svg" />
