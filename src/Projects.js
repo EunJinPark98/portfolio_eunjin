c8c0684cebd192feba83c7bce817875601d8d1d6
EJP <obliviscor29@naver.com>
2023-11-08 02:00:46 +0900

    그린모아

--- src/Projects.js
+++ src/Projects.js
@@ -113,9 +113,9 @@ function Projects(){
                 </Col>
                 <Col>
                     <div>
-                        <img className="greenMoa" onClick={() => {alert('아직 제작중입니다!')}} src="https://eunjinpark98.github.io/portfolio_eunjin/GreenMoa.png" />
+                        <img className="greenMoa" onClick={() => {window.open('https://chocolate-candy-d12.notion.site/GREEN-MOA-6f38ea7751fb4dbebf8b5058bef14e99')}} src="https://eunjinpark98.github.io/portfolio_eunjin/GreenMoa.png" />
                     </div>
-                    <img className="project-view" onClick={()=>{alert('아직 제작중입니다!')}} src="https://eunjinpark98.github.io/portfolio_eunjin/cursor_click.png"/>
+                    <img className="project-view" onClick={()=>{window.open('https://chocolate-candy-d12.notion.site/GREEN-MOA-6f38ea7751fb4dbebf8b5058bef14e99')}} src="https://eunjinpark98.github.io/portfolio_eunjin/cursor_click.png"/>
                 </Col>
             </Row>
             <Row className="project-box mb-5">
